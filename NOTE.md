283491325dde1b4fad272b4343a1db8d9961502d
joe Huang <qwert30830@gmail.com>
2022-06-20 11:50:04 +0800

    first commit

---
+++ NOTE.md
@@ -0,0 +1,4 @@
+### env
+```js
+declare module '*.vue'; // env.d.ts
+```
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import TheWelcome from "@/components/TheWelcome.vue";
+import { useCounterStore } from "@/stores/counter";
+
+const counterStore = useCounterStore();
+const update = () => {
+  counterStore.counter = minus(10, 5);
+};
+
+function minus(a: number, b: number) {
+  return a - b;
+}
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <div>
+      {{ counterStore.counter }}
+    </div>
+    <Button @click="update">update</Button>
+  </main>
+</template>
