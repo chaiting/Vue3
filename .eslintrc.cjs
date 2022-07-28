e135fc25ab585e7b605085b81daeb2a6ca82313f
joe Huang <qwert30830@gmail.com>
2022-07-28 14:36:43 +0800

    remove cypress

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,12 +9,6 @@ module.exports = {
     "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
   ],
-  overrides: [
-    {
-      files: ["cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"],
-      extends: ["plugin:cypress/recommended"],
-    },
-  ],
   rules: {
     "vue/multi-word-component-names": "off",
     "prefer-const": "off",
