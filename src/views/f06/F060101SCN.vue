c927d38a21c31bb93159c61a01d8e1ba17dd30cb
joe Huang <qwert30830@gmail.com>
2022-10-17 17:35:07 +0800

    update

--- src/views/f06/F060101SCN.vue
+++ src/views/f06/F060101SCN.vue
@@ -1,32 +1,62 @@
 <template>
+  <h3>限制特定日期</h3>
   <Space size="large" wrap>
     <DatePicker
       type="date"
       placeholder="起始日期"
       format="yyyy/MM/dd"
-      :options="restrictSpecificSDate(form.eDate)"
-      @on-change="form.sDate = $event"
+      :options="restrictSpecificSDate(form1.eDate)"
+      @on-change="form1.sDate = $event"
       style="width: 200px"
     />
     <DatePicker
       type="date"
       placeholder="結束日期"
       format="yyyy/MM/dd"
-      :options="restrictSpecificEDate(form.sDate)"
-      @on-change="form.eDate = $event"
+      :options="restrictSpecificEDate(form1.sDate)"
+      @on-change="form1.eDate = $event"
       style="width: 200px"
     />
   </Space>
-  <pre>{{ form }}</pre>
+  <pre>{{ form1 }}</pre>
+  <h3>限制特定區間</h3>
+  <Space size="large" wrap>
+    <DatePicker
+      type="date"
+      placeholder="起始日期"
+      format="yyyy/MM/dd"
+      :options="restrictPeriodSDate(form2.eDate, 'd', 10)"
+      @on-change="form2.sDate = $event"
+      style="width: 200px"
+    />
+    <DatePicker
+      type="date"
+      placeholder="結束日期"
+      format="yyyy/MM/dd"
+      :options="restrictPeriodEDate(form2.sDate, 'd', 10)"
+      @on-change="form2.eDate = $event"
+      style="width: 200px"
+    />
+  </Space>
+  <pre>{{ form2 }}</pre>
 </template>
 <script setup lang="ts">
 import { reactive } from "vue";
 import {
   restrictSpecificSDate,
   restrictSpecificEDate,
+  restrictPeriodSDate,
+  restrictPeriodEDate,
 } from "@/core/utils/date-restrict-utils";
 
-const form = reactive({
+// 表單1
+const form1 = reactive({
+  sDate: "",
+  eDate: "",
+});
+
+// 表單2
+const form2 = reactive({
   sDate: "",
   eDate: "",
 });
